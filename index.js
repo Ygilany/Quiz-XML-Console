4f9319740f16d5cfe2ca1d5243d2a8fc4162a61d
Yahya Gilany <gilanyym@ucmail.uc.edu>
2020-06-04 13:13:34 -0400

    more logs

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const result = convert.xml2json(fs.readFileSync(`./${process.argv[2]}`), {compac
 const jsonQuiz = JSON.parse(result);
 
 for (let group of jsonQuiz.quiz.group) {
+  console.log(`-------------------------------------------------------`)
   console.log(`Questions Group: ${group._attributes.id}`)
+  console.log(`-------------------------------------------------------`)
+
   for(let question of group.question) {
-    console.log(`➡ Q.${question.id._text} - type: ${question.type._text} - ${question.text._text}`);
+    console.log(`➡ Q.${question.id._text} - (${question.type._text}) - ${question.text._text}`);
     for(let answer of question.answer) {
-      console.log(`➡➡${answer._text}. isCorrect? ${answer._attributes.correct}`)
+      console.log(`➡➡${answer._text}. ${answer._attributes.correct === `true` ? "<<" : ""}`)
     }
+    console.log()
   }
-  console.log(`=======================================================`)
 }
